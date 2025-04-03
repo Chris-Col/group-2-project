d928695a3dfb6d5e33e2303a63daf0030cb2dc6a
Hunor Kovacs <92712055+hunorkov@users.noreply.github.com>
2025-04-03 12:58:42 -0600

    Delete ProjectSourceCode/init_data/init.sql

--- ProjectSourceCode/init_data/init.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Users (username, email, pw, created_at, last_login)
-VALUES (
-  'hunorko',
-  'huko2764@colorado.edu',
-  -- crypt('securepassword', gen_salt('bf')),
-  'huko2764',
-  CURRENT_TIMESTAMP,
-  CURRENT_TIMESTAMP
-);
